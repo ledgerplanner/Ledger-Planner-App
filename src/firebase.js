3a8e09c4bcc488259e25c17fac82942f753a100a
ledgerplanner <ledgerplanner@gmail.com>
2026-04-10 11:25:19 -0400

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDRtXAjd-2KpZOlQL-bWrGoz6S3HuK4jDI",
+  authDomain: "ledger-planner-38ab7.firebaseapp.com",
+  projectId: "ledger-planner-38ab7",
+  storageBucket: "ledger-planner-38ab7.firebasestorage.app",
+  messagingSenderId: "624261529539",
+  appId: "1:624261529539:web:80aec4cca266a3a6008776"
+};
+
+// Initialize Firebase once
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };

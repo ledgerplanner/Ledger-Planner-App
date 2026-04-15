1818ea191910a361b425eb75c254beaf6bcc456e
ledgerplanner <ledgerplanner@gmail.com>
2026-04-15 14:37:09 -0400

    Create firebase-messaging-sw.js

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,27 @@
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
+
+// Using your exact config
+const firebaseConfig = {
+  apiKey: "AIzaSyDRtXAjd-2KpZOlQL-bWrGoz6S3HuK4jDI",
+  authDomain: "ledger-planner-38ab7.firebaseapp.com",
+  projectId: "ledger-planner-38ab7",
+  storageBucket: "ledger-planner-38ab7.firebasestorage.app",
+  messagingSenderId: "624261529539",
+  appId: "1:624261529539:web:80aec4cca266a3a6008776"
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+// Handles background notifications
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/app-icon.png' // Matches your public folder icon
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
